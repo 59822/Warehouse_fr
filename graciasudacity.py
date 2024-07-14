f7a1a25d9ac4f1b70e19bdbbf46a394934bf9258
Kathe <catemoreno33@gmail.com>
2024-07-14 02:03:18 -0500

    Solo hice q validara con strip() la parte de espacios en blanco ok

--- graciasudacity.py
+++ graciasudacity.py
@@ -1,35 +1,38 @@
-def main_app():
-    import json
+from jasd import main_app
+# Importar la clase Producto desde el archivo clases.py
+from clases import Producto
 
-    try:
-        with open('user.json', 'r') as file:
-            user_alm = json.load(file)
-    except FileNotFoundError:
-        user_alm = {}
+# Lista para mantener un registro de productos existentes en el inventario
+productos_existentes = []
+main_app()
 
-    status = None
-    while status != 'exit':
-        print("\n__________________________________________________________\n\nBienvenido a la aplicación. A continuación seleccione una opción:")
-        choice = int(input("1. Registrarse en el sistema\n2. Iniciar sesión\n"))
-
-        if choice == 1:
-            username, password = input("Ingrese su nombre de usuario y contraseña: ").split()
-            if username in user_alm:
-                print("El nombre de usuario ya existe.")
-            else:
-                user_alm[username] = password
+status = None
+while status != 'exit':
+    print("\n__________________________________________________________")
+    print("\n\nGracias por usar la aplicación. Seleccione las siguientes opciones para habilitar inventario: ")
+    choice = int(input("1. Agregar algo al inventario\n2. Salir\n"))
+    
+    if choice == 1:
+        product_name = input("Ingrese el nombre del producto: ") 
+        product_code = input("Ingrese el código del producto: ")
+        
+        # Generar un nombre único para el producto
+        nombre_unico = f"{product_name.strip()}--{product_code.strip()}"# Eliminar espacios en blanco al principio
+        productos_existentes
+        # Verificar si el producto ya existe en el inventario
+        if nombre_unico in productos_existentes:
+            print("\nEl producto ya existe en el inventario.")
+            continue
+        else:
+            # Obtener información adicional del producto
+            product_category = input("Ingrese la categoría del producto: ")
+            product_type = input("Ingrese el tipo de producto: ")
+            product_brand = input("Ingrese la marca del producto: ")
+            product_supplier = input("Ingrese el proveedor del producto: ")
+            product_description = input("Ingrese la descripción del producto: ")
             
-            # Guardar el diccionario actualizado en el archivo JSON
-            with open('users.json', 'w') as f:
-                json.dump(user_alm, f, indent=4)
-        
-        elif choice == 2:
-            # Buscar en el archivo o en el diccionario
-            user_log1, pass_log1 = input("Ingrese su nombre de usuario y contraseña: ").split()
-            if user_log1 in user_alm and user_alm[user_log1] == pass_log1:
-                print("Inicio de sesión exitoso.")
-            else:
-                print("Usuario o contraseña incorrectos.")
-        
-
-    print("Gracias por usar la aplicación.")
+            # Crear una nueva instancia de Producto
+            producto_new = Producto(product_name, product_code, product_category, product_type, product_brand, product_supplier, product_description)
+            
+            # Agregar el nombre único del producto a la lista de productos existentes
+            productos_existentes.append(nombre_unico)
